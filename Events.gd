be9f7ffee98469591d8f04378fc74bd7c2f23f9a
chloechen436 <chchen16@s.sfusd.edu>
2024-03-21 10:49:14 -0700

    Temp. Duck and heart added

--- Events.gd
+++ Events.gd
@@ -1,3 +1,5 @@
 extends Node
 
-signal level_completed
+
+
+signal hearts_collected
--- heart.gd
+++ heart.gd
@@ -4,5 +4,5 @@ func _on_body_entered(body):
 	queue_free()
 	var hearts = get_tree().get_nodes_in_group("Hearts")
 	if hearts.size() == 1:
-		Events.level_completed.emit()
+		Events.hearts_collected.emit()
 
