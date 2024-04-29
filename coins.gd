0df45c37d0aad07f2894fc437423541604355a5a
chloechen436 <chchen16@s.sfusd.edu>
2024-04-29 15:12:19 -0700

    Coin Sound Added

--- coins.gd
+++ coins.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+@onready var coin_collected_sound = $"../../CoinCollectedSound"
+
 func _on_body_entered(body):
 	queue_free()
 	var coins = get_tree().get_nodes_in_group("Coins")
 	if coins.size() == 1:
-		Events.level_completed.emit() 
+		Events.level_completed.emit()
+	coin_collected_sound.play()
