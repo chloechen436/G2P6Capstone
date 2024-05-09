4be18556cd9b6a98a8b40f7b494bff9522b4ade3
Jtotheazzy <jahuang7@s.sfusd.edu>
2024-05-09 15:42:13 -0700

    update door

--- door.gd
+++ door.gd
@@ -13,7 +13,10 @@ func _process(delta):
 
 
 func _on_player_check_area_body_entered(body):
+	print("collisio with: " + body.name)
+	print("checking all coins")
 	if are_coins_collected():
+		print("no coins found, destorying door")
 		#if all the coins are collected, destroy this door.
 		destroy_door()
 	pass # Replace with function body.
@@ -22,7 +25,9 @@ func _on_player_check_area_body_entered(body):
 func are_coins_collected():
 	for coin in linked_coins:
 		if coin != null:
+			print("coin " + coin.name + "is not null")
 			return false
+	print("no coins found")
 	return true
 
 #destroy the door
