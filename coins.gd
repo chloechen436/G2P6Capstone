ac8bd5b9839d08deb83bf24d5b44f13896d5947d
Jtotheazzy <jahuang7@s.sfusd.edu>
2024-05-15 12:41:05 -0700

    Revert "."
    This reverts commit 879536213c69bcb00bdef26639b34c40a4002829, reversing
    changes made to c3017a7e45e3c9936cfc9b23accee5b807e40d6d.

--- coins.gd
+++ coins.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
-#@onready var coin_collected_sound = $"../../CoinCollectedSound"
-@onready var coin_collected = $"../../CoinCollected"
+@onready var coin_collected_sound = $"../../CoinCollectedSound"
 
 func _on_body_entered(body):
 	queue_free()
 	var coins = get_tree().get_nodes_in_group("Coins")
 	if coins.size() == 1:
 		Events.level_completed.emit()
-	coin_collected.play()
+	coin_collected_sound.play()
